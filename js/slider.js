588b7f229feb3047c119dcc02cf5909bfd8c7024
Jean Pool Gutierrez <jean-gutierrez-117@hotmail.com>
2023-08-10 23:57:23 -0500

    19. Finalizando funcionalidad del Slider

--- js/slider.js
+++ js/slider.js
@@ -16,8 +16,12 @@
         const currenTestimony = document.querySelector('.testimony__person--show').dataset.id;
         value = Number(currenTestimony);
         value += add;
+
+        sliders[Number(currenTestimony)-1].classList.remove('testimony__person--show');
         if(value === sliders.length+1 || value === 0 ){
             value = value === 0? sliders.length : 1;
         }
+
+        sliders[value-1].classList.add('testimony__person--show');
     }
 })();
